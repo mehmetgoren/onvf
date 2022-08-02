286a05040fa0b64219a5b723da0063e0ab0b98df
gokalp <mehmetgoren@outlook.com>
2022-08-02 20:04:58 +0300

    Adds Docker support part II

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM golang:1.18.4
 
-WORKDIR /usr/src/app
-
 RUN mkdir /app
 
 ADD . /app
--- reps/service_repository.go
+++ reps/service_repository.go
@@ -9,20 +9,28 @@ import (
 	"time"
 )
 
+type InstanceType int
+
+const (
+	systemd   InstanceType = 0
+	container InstanceType = 1
+)
+
 type ServiceModel struct {
-	Name            string `json:"name" redis:"name"`
-	Description     string `json:"description" redis:"description"`
-	Platform        string `json:"platform" redis:"platform"`
-	PlatformVersion string `json:"platform_version" redis:"platform_version"`
-	HostName        string `json:"hostname" redis:"hostname"`
-	IpAddress       string `json:"ip_address" redis:"ip_address"`
-	MacAddress      string `json:"mac_address" redis:"mac_address"`
-	Processor       string `json:"processor" redis:"processor"`
-	CpuCount        int    `json:"cpu_count" redis:"cpu_count"`
-	Ram             string `json:"ram" redis:"ram"`
-	Pid             int    `json:"pid" redis:"pid"`
-	CreatedAt       string `json:"created_at" redis:"created_at"`
-	Heartbeat       string `json:"heartbeat" redis:"heartbeat"`
+	Name            string       `json:"name" redis:"name"`
+	Description     string       `json:"description" redis:"description"`
+	Platform        string       `json:"platform" redis:"platform"`
+	PlatformVersion string       `json:"platform_version" redis:"platform_version"`
+	HostName        string       `json:"hostname" redis:"hostname"`
+	IpAddress       string       `json:"ip_address" redis:"ip_address"`
+	MacAddress      string       `json:"mac_address" redis:"mac_address"`
+	Processor       string       `json:"processor" redis:"processor"`
+	CpuCount        int          `json:"cpu_count" redis:"cpu_count"`
+	Ram             string       `json:"ram" redis:"ram"`
+	Pid             int          `json:"pid" redis:"pid"`
+	InstanceType    InstanceType `json:"instance_type" redis:"instance_type"`
+	CreatedAt       string       `json:"created_at" redis:"created_at"`
+	Heartbeat       string       `json:"heartbeat" redis:"heartbeat"`
 }
 
 type ServiceRepository struct {
@@ -48,6 +56,7 @@ func (r *ServiceRepository) Add(serviceName string) (int64, error) {
 		CpuCount:        runtime.NumCPU(),
 		Ram:             "unknown",
 		Pid:             os.Getpid(),
+		InstanceType:    container,
 		CreatedAt:       utils.TimeToString(now, true),
 		Heartbeat:       "",
 	}
